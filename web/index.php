ba76bbf1fb15aff4e58ae235d578bda13cbf23ed
Paul Waring <paul@xk7.net>
2013-07-11 08:45:46 +0100

    Set debugging to on by default.

--- web/index.php
+++ web/index.php
@@ -8,6 +8,9 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views',
 ));
 
+// Debugging is on by default - you may wish to change this on a per-host basis
+$app['debug'] = true;
+
 // definitions
 
 $app->run();
